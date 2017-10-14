e556621cbb0788f9159d9ee3a9b188c07ace9dea
barbara111 <seferynska@gmail.com>
2017-10-14 17:28:08 +0200

    Add files via upload

---
+++ PWM.ino
@@ -0,0 +1,93 @@
+int AHI = 7;
+int BHI = 8;
+int ALI = 6;
+int BLI = 5;
+int D = 4;
+
+
+void setup() {
+  // wyjścia
+  pinMode(AHI, OUTPUT);
+  pinMode(ALI, OUTPUT);
+  pinMode(BHI, OUTPUT);
+  pinMode(BLI, OUTPUT);
+  pinMode(D, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(D, LOW);
+
+}
+// przełączanie sygnałów parami według schematu układu
+void lewo() {
+  digitalWrite(ALI, LOW);
+  digitalWrite(BHI, LOW); //brak sygnału podawanego na ALI i BHI
+  for (int i=0; i<=255; i++) //PWM na BLI i AHI, przyspieszanie silnika
+  {
+    analogWrite(AHI, i);
+    analogWrite(BLI, i);
+    delay(20);
+  }
+  delay(1000);
+  for (int i=255; i>=0; i--) //zwalnianie silnika
+  {
+    analogWrite(AHI, i);
+    analogWrite(BLI, i);
+    delay(20);
+  }
+  delay(1000);
+}
+void prawo() {
+  digitalWrite(AHI, LOW);
+  digitalWrite(BLI, LOW); //brak sygnału podawanego na AHI i BLI
+  for (int i=0; i<=255; i++) //PWM na BHI i ALI, przyspieszanie silnika
+  {
+    analogWrite(BHI, i);
+    analogWrite(ALI, i);
+    delay(20);
+  }
+  delay(1000);
+  for (int i=255; i>=0; i--) //zwalnianie silnika
+  {
+    analogWrite(BHI, i);
+    analogWrite(ALI, i);
+    delay(20);
+  }
+  delay(1000);
+}
+//przełączanie sygnałów według znalezionej tabeli prawdy
+void lewotab() {
+  digitalWrite(AHI, HIGH);
+  digitalWrite(BHI, LOW);
+  digitalWrite(ALI, LOW);
+  for (int i=0; i<=255; i++) //PWM na BLI, przyspieszanie silnika
+  {
+    analogWrite(BLI, i);
+    delay(20);
+  }
+  delay(1000);
+  for (int i=255; i>=0; i--) //zwalnianie silnika
+  {
+    analogWrite(BLI, i);
+    delay(20);
+  }
+  delay(1000);
+}
+void prawotab() {
+  digitalWrite(AHI, LOW);
+  digitalWrite(BHI, HIGH);
+  digitalWrite(BLI, LOW);
+  for (int i=0; i<=255; i++) //PWM na ALI, przyspieszanie silnika
+  {
+    analogWrite(ALI, i);
+    delay(20);
+  }
+  delay(1000);
+  for (int i=255; i>=0; i--) //zwalnianie silnika
+  {
+    analogWrite(ALI, i);
+    delay(20);
+  }
+  delay(1000);
+}
+
